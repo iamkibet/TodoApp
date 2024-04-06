b4d56d33fd24998bda55e1d97c370eec64615cf2
iamkibet <100912353+iamkibet@users.noreply.github.com>
2024-04-06 22:23:37 +0300

    Added a Note app and a button to toggle between the two

--- script.js
+++ script.js
@@ -2,7 +2,85 @@ const inputBox = document.getElementById("input-box");
 const taskList = document.getElementById("task-list");
 const completedCounter = document.getElementById("completed-counter");
 const uncompletedCounter = document.getElementById("uncompleted-counter");
+const todoBtn = document.getElementById("todobtn");
+const notesBtn = document.getElementById("notesbtn");
+const todoSection = document.getElementById("todosection");
+const notesSection = document.getElementById("notessection");
+const background = document.getElementById("background");
+const btnEl = document.getElementById("btn");
+const appEl = document.getElementById("app");
 
+getNotes().forEach((note) => {
+  const noteEl = createNoteEl(note.id, note.content);
+  appEl.insertBefore(noteEl, btnEl);
+});
+
+function createNoteEl(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Click to write";
+  element.value = content;
+
+  element.addEventListener("dblclick", () => {
+    const warning = confirm("Delete this note?");
+    if (warning) {
+      deleteNote(id, element);
+    }
+  });
+
+  element.addEventListener("input", () => {
+    updateNote(id, element.value);
+  });
+
+  return element;
+}
+function deleteNote(id, element) {
+  const notes = getNotes().filter((note) => note.id != id);
+  SaveNote(notes);
+  appEl.removeChild(element);
+}
+
+function updateNote(id, content) {
+  const notes = getNotes();
+  const taget = notes.filter((note) => note.id == id)[0];
+  taget.content = content;
+  SaveNote(notes);
+}
+
+function addNote() {
+  const notes = getNotes();
+  const noteObj = {
+    id: Math.floor(Math.random() * 100000),
+    content: "",
+  };
+
+  const noteEl = createNoteEl(noteObj.id, noteObj.content);
+
+  appEl.insertBefore(noteEl, btnEl);
+
+  notes.push(noteObj);
+
+  SaveNote(notes);
+}
+
+function SaveNote(note) {
+  localStorage.setItem("note-app", JSON.stringify(note));
+}
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("note-app") || "[]");
+}
+btnEl.addEventListener("click", addNote);
+
+todoBtn.addEventListener("click", function () {
+  todoSection.classList.remove("hidden");
+  notesSection.classList.add("hidden");
+});
+
+notesBtn.addEventListener("click", () => {
+  notesSection.classList.remove("hidden");
+  todoSection.classList.add("hidden");
+});
 // Update counters function
 function updateCounters() {
   const completedTasks = document.querySelectorAll(".completed").length;
@@ -38,8 +116,8 @@ function loadTasksFromLocalStorage() {
         <input type="checkbox" ${task.completed ? "checked" : ""} />
         <span>${task.text}</span>
       </label>
-      <span class="edit-btn">edit</span>
-      <span class="delete-btn">delete</span>`;
+      <span class="edit-btn">Edit</span>
+      <span class="delete-btn">Delete</span>`;
       taskList.appendChild(li);
 
       const checkbox = li.querySelector("input");
@@ -88,8 +166,8 @@ function addTask() {
     <input type="checkbox" />
     <span>${task}</span>
   </label>
-  <span class="edit-btn">edit</span>
-  <span class="delete-btn">delete</span>`;
+  <span class="edit-btn">Edit</span>
+  <span class="delete-btn">Delete</span>`;
   taskList.appendChild(li);
   inputBox.value = "";
 
